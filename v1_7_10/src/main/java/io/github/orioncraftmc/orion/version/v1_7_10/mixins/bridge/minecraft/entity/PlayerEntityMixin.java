3acd465ea94946cdf66dda7d08590309c5286848
SD <harrypotter.starwars.shrish@gmail.com>
2021-09-25 13:01:34 +0530

    Implement player inventory accessor

---
+++ v1_7_10/src/main/java/io/github/orioncraftmc/orion/version/v1_7_10/mixins/bridge/minecraft/entity/PlayerEntityMixin.java
@@ -0,0 +1,23 @@
+package io.github.orioncraftmc.orion.version.v1_7_10.mixins.bridge.minecraft.entity;
+
+import io.github.orioncraftmc.orion.api.bridge.minecraft.entity.EntityPlayerBridge;
+import io.github.orioncraftmc.orion.api.bridge.minecraft.item.inventory.PlayerInventoryBridge;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+
+@Mixin(EntityPlayer.class)
+public class PlayerEntityMixin implements EntityPlayerBridge {
+
+	@Shadow
+	public InventoryPlayer inventory;
+
+	@NotNull
+	@Override
+	public PlayerInventoryBridge getPlayerInventory() {
+		return (PlayerInventoryBridge) this.inventory;
+	}
+}
