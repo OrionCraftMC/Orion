bafabc15b1a5fbc04305edf36cc2d75c2f0f3093
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-12-07 02:21:45 +0000

    Disable face culling when rendering the player's hand

--- v1_5_2/src/main/java/io/github/orioncraftmc/orion/version/v1_5_2/mixins/backport/skins/render/RenderPlayerMixin.java
+++ v1_5_2/src/main/java/io/github/orioncraftmc/orion/version/v1_5_2/mixins/backport/skins/render/RenderPlayerMixin.java
@@ -20,15 +20,14 @@ package io.github.orioncraftmc.orion.version.v1_5_2.mixins.backport.skins.render
 import io.github.orioncraftmc.orion.version.v1_5_2.backport.skins.OrionModelPlayer;
 import io.github.orioncraftmc.orion.version.v1_5_2.backport.skins.ducks.EntityPlayerGameProfileDuck;
 import io.github.orioncraftmc.orion.version.v1_5_2.backport.skins.ducks.RenderPlayerDuck;
-import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBiped;
-import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.client.model.*;
 import net.minecraft.client.render.RenderPlayer;
 import net.minecraft.entity.player.EntityPlayer;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RenderPlayer.class)
 public class RenderPlayerMixin implements RenderPlayerDuck {
@@ -47,6 +46,15 @@ public class RenderPlayerMixin implements RenderPlayerDuck {
 
 		if (v == 1.0) v = 0.75f;
 		return new ModelBiped(v);
+	}
+	@Inject(method = "renderFirstPersonArm", at = @At("RETURN"))
+	private void onRenderArmEnd(CallbackInfo info) {
+		GL11.glEnable(GL11.GL_CULL_FACE);
+	}
+
+	@Inject(method="renderFirstPersonArm", at=@At("HEAD"))
+	protected void onRenderArmStart(EntityPlayer par1, CallbackInfo ci) {
+		GL11.glDisable(GL11.GL_CULL_FACE);
 	}
 
 	@Redirect(method = {"renderPlayer", "renderSpecials", "renderFirstPersonArm"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/RenderPlayer;modelBipedMain:Lnet/minecraft/client/model/ModelBiped;"))
