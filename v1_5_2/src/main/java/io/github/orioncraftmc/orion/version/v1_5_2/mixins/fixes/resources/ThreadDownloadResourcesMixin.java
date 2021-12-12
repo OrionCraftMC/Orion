0f19c27dd18ae51ee3a40985225caa26efb42d8a
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-12-12 09:55:00 +0000

    Force load of in-game sounds

--- v1_5_2/src/main/java/io/github/orioncraftmc/orion/version/v1_5_2/mixins/fixes/resources/ThreadDownloadResourcesMixin.java
+++ v1_5_2/src/main/java/io/github/orioncraftmc/orion/version/v1_5_2/mixins/fixes/resources/ThreadDownloadResourcesMixin.java
@@ -19,6 +19,7 @@ package io.github.orioncraftmc.orion.version.v1_5_2.mixins.fixes.resources;
 
 import net.minecraft.ThreadDownloadResources;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -26,10 +27,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ThreadDownloadResources.class)
 public abstract class ThreadDownloadResourcesMixin {
 
+	@Shadow
+	public abstract void reloadResources();
+
 	@Inject(method = "run", at = @At("HEAD"), cancellable = true)
 	public void onRun(CallbackInfo ci) {
 		// Resources are downloaded by the launchers now
 		ci.cancel();
+		// Force reloading of resources because they are already downloaded and ready to use
+		reloadResources();
 	}
 
 }
